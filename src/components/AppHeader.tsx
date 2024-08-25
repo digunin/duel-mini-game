381de2becba4ba6250557bf066240e760d9a5730
digunin <kandidad@gmail.com>
2024-08-26 02:30:50 +0300

    buttons for change game status

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { useAppContext } from "../hooks/useAppContext";
+import { GameStatus } from "../store/types";
+import { setGameStatus } from "../store/actions";
 
 export const AppHeader = () => {
-  return <header></header>;
+  const {
+    dispatch,
+    state: { gameStatus },
+  } = useAppContext();
+
+  const callback = (status: GameStatus) => () => {
+    dispatch(setGameStatus(status));
+  };
+
+  return (
+    <header>
+      <button
+        disabled={gameStatus !== GameStatus.IDLE}
+        onClick={callback(GameStatus.RUNNING)}
+      >
+        Начать игру
+      </button>
+      <button
+        disabled={gameStatus === GameStatus.IDLE}
+        onClick={callback(GameStatus.IDLE)}
+      >
+        Закончить
+      </button>
+      <button
+        disabled={gameStatus === GameStatus.IDLE}
+        onClick={callback(
+          gameStatus === GameStatus.PAUSED
+            ? GameStatus.RUNNING
+            : GameStatus.PAUSED
+        )}
+      >
+        {gameStatus === GameStatus.PAUSED ? "Продолжить" : "Пауза"}
+      </button>
+    </header>
+  );
 };
