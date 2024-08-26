bbf497b84b9f69317f7465af6a1f57b6864909d7
digunin <kandidad@gmail.com>
2024-08-26 13:08:02 +0300

    App component refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,13 @@ import { useAppScreen } from "./hooks/useAppScreen";
 import { AppHeader } from "./components/AppHeader";
 import { useHeroControl } from "./hooks/useHeroControl";
 import { HeroMenu } from "./components/HeroMenu";
-import { Point } from "./components/game/game-units/primitives/Point";
-import { useAppContext } from "./hooks/useAppContext";
-import { HeroSide } from "./components/game/Game";
-import { setLeftHero, setRightHero } from "./store/actions";
-import { HeroClickHandler } from "./hooks/useGame";
+import { useHeroMenu } from "./hooks/useHeroMenu";
 
 function App() {
   const { orient } = useAppScreen();
   const { left, right, setHeroProps } = useHeroControl();
-  const { dispatch } = useAppContext();
-  const [menuPosition, setMenuPosition] = useState<Point | null>(null);
-  const [activeHero, setActiveHero] = useState<HeroSide>("left");
-
-  const MENU_WIDTH = 100;
-  const MENU_HEIGHT = 200;
-
-  const onColorChangeHandler = (spellColor: string) => {
-    if (activeHero === "left") {
-      dispatch(setLeftHero({ spellColor }));
-    } else {
-      dispatch(setRightHero({ spellColor }));
-    }
-    setMenuPosition(null);
-  };
-
-  const onHeroClicklHandler: HeroClickHandler = (point, hero, canvasRect) => {
-    if (!hero) {
-      setMenuPosition(null);
-      return;
-    }
-    const menuPosition = new Point(
-      Math.min(point.x, canvasRect!.right - MENU_WIDTH - 10 || point.x),
-      Math.min(point.y, canvasRect!.bottom - MENU_HEIGHT || point.y)
-    );
-    setMenuPosition(menuPosition);
-    setActiveHero(hero);
-  };
+  const { menuPosition, onColorChangeHandler, onHeroClicklHandler } =
+    useHeroMenu();
 
   if (orient == "portrait") {
     return (
